73b58b46d48fb5ddbdaab3c2bdee210d666d5bd9
tu pham <panhtu0902@gmail.com>
2024-07-03 18:51:33 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./assets/components/Sidebar";
+import Home from "./assets/pages/Home";
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
