1b5f90f5c5145c1dcc2d27af02c97dd08255fdc8
tu pham <panhtu0902@gmail.com>
2024-07-05 15:31:23 +0700

    feat: update UI in sidebar

--- src/assets/components/Sidebar.jsx
+++ src/assets/components/Sidebar.jsx
@@ -1,9 +1,37 @@
-function Sidebar() {
-  return <>
-    <div className="container max-w-1">
+import classNames from "classnames";
+import logo from "./../logo.png";
+import { FiAlignJustify } from "react-icons/fi";
 
-    </div>
-  </>
+function Sidebar() {
+  return (
+    <nav className={classNames("icon", "site-navigation", "fixed")}>
+      <div className="navigation-header flex p-6 bg-sky-900 items-center">
+        <a href="/" className="w-20">
+          <img src={logo} alt="logo" />
+        </a>
+        <button className="inline-flex p-0 m-0 ml-2.5 h-fit bg-transparent focus:outline-none">
+          <FiAlignJustify />
+        </button>
+      </div>
+      <ul className="navigation-body">
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>About</a>
+        </li>
+        <li>
+          <a>Skill</a>
+        </li>
+        <li>
+          <a>Project</a>
+        </li>
+        <li>
+          <a>Education</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Sidebar;
--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,7 +1,11 @@
 function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="container w-full max-w-4xl pt-36 pb-8 mx-auto">
+        <h1>Hi, there</h1>
+        <h2>I'm Anh Tu Pham</h2>
+        <p>Web developer</p>
+      </div>
     </>
   );
 }
