fbe09b08beb2da1addc040a47ece9208462e282e
tu pham <panhtu0902@gmail.com>
2024-08-05 02:59:11 +0700

    refactor: responsive

--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -13,14 +13,26 @@ function Home() {
       <ParticlesContainer />
       <section
         id="home"
-        className={`h-full w-full pt-36 pb-8 flex flex-wrap `}
+        className="h-full w-full mt-36 mb-8 flex flex-wrap max-lg:mt-28 max-lg:mb-4 max-sm:mt-2 max-sm:mb-0"
       >
         <div className="max-w-4xl w-2/3 mx-auto flex flex-col items-center gap-2.5 text-white mt-12">
-          <h1 className="text-[40px] capitalize text-left max-md:text-[30px]">Hi, there</h1>
-          <h1 className="text-[42px] uppercase max-md:text-[31px]">I'm
-            <span className="text-fuchsia-600 ml-3 font-semibold">Anh Tu Pham</span>
+          <h1 className="text-[40px] capitalize text-left max-md:text-[30px] max-sm:text-[20px]">
+            Hi, there
           </h1>
-          <TypeWriter speed="150" texts={['Front-end Developer', 'Back-end Developer', 'Intern Developer']}/>
+          <h1 className="text-[42px] uppercase max-md:text-[31px] max-sm:text-[19px]">
+            I'm
+            <span className="text-fuchsia-600 ml-3 font-semibold">
+              Anh Tu Pham
+            </span>
+          </h1>
+          <TypeWriter
+            speed="150"
+            texts={[
+              "Front-end Developer",
+              "Back-end Developer",
+              "Intern Developer",
+            ]}
+          />
 
           {/* icon brand */}
           <div>
